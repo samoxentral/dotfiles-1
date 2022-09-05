1420361bceb64bb8a5adc62ad56ea8f96bd67e26
Vladislav Shavelskiy <v.shavelskiy@192.168.1.3>
2022-09-05 13:02:59 +0300

    update config

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -7,6 +7,7 @@ return require('packer').startup(function()
   use 'lewis6991/gitsigns.nvim'
   use 'dinhhuy258/git.nvim'
 
+  -- other
   use 'goolord/alpha-nvim'
   use 'windwp/nvim-autopairs'
   use 'tpope/vim-surround'
@@ -16,13 +17,16 @@ return require('packer').startup(function()
   use 'rmagatti/auto-session'
   use 'karb94/neoscroll.nvim'
 
+  -- tree
   use 'kyazdani42/nvim-web-devicons'
   use 'kyazdani42/nvim-tree.lua'
 
+  -- treesitter
   use 'nvim-treesitter/nvim-treesitter'
   use 'nvim-treesitter/playground'
   use 'mtdl9/vim-log-highlighting'
 
+  -- telescope
   use 'nvim-telescope/telescope.nvim'
   use 'nvim-telescope/telescope-file-browser.nvim'
   use 'nvim-lua/plenary.nvim'
@@ -36,17 +40,19 @@ return require('packer').startup(function()
   use 'williamboman/mason.nvim'
   use 'williamboman/mason-lspconfig.nvim'
 
-  -- lsp completion
+  -- snippets
   use 'rafamadriz/friendly-snippets'
   use 'saadparwaiz1/cmp_luasnip'
   use 'L3MON4D3/LuaSnip'
 
+  -- lsp completion
   use 'hrsh7th/nvim-cmp'
   use 'hrsh7th/cmp-nvim-lsp'
   use 'hrsh7th/cmp-buffer'
   use 'hrsh7th/cmp-path'
   use 'hrsh7th/cmp-nvim-lua'
 
+  -- hightlight lsp word
   use 'RRethy/vim-illuminate'
 
   -- dap
