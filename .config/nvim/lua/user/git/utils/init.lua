fa8ed7af9a0587b364be5a4f9bdd4fffb87f19f3
Vladislav Shavelskiy <v.shavelskiy@192.168.1.3>
2022-09-16 14:59:12 +0300

    update config

--- .config/nvim/lua/user/git/utils/init.lua
+++ .config/nvim/lua/user/git/utils/init.lua
@@ -10,7 +10,7 @@ end
 
 function M.split(s, delimiter)
   local result = {}
-  for match in (s .. delimiter):gmatch("(.-)" .. delimiter) do
+  for match in (s .. delimiter):gmatch('(.-)' .. delimiter) do
     table.insert(result, match)
   end
 
