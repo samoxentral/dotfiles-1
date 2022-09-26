28746aeb96e1d82ca618beb92dcdf348d59c8119
Vladislav Shavelskiy <v.shavelskiy@192.168.1.3>
2022-09-26 11:11:32 +0300

    update config

--- .config/nvim/lua/colors/treesitter/html.lua
+++ .config/nvim/lua/colors/treesitter/html.lua
@@ -1,9 +1,9 @@
 local set_hl = vim.api.nvim_set_hl
 local colors = require 'colors.colors'
 
-set_hl(0, 'htmlTag', { fg = colors.base0C})
-set_hl(0, 'htmlEndTag', { fg = colors.base0C})
-set_hl(0, 'htmlTagN', { fg = colors.base0C})
-set_hl(0, 'htmlTagName', { fg = colors.base0C})
-set_hl(0, 'htmlArg', { fg = colors.base0B})
-set_hl(0, 'htmlString', { fg = colors.base0A})
+set_hl(0, 'htmlTag', { fg = colors.base0C })
+set_hl(0, 'htmlEndTag', { fg = colors.base0C })
+set_hl(0, 'htmlTagN', { fg = colors.base0C })
+set_hl(0, 'htmlTagName', { fg = colors.base0C })
+set_hl(0, 'htmlArg', { fg = colors.base0B })
+set_hl(0, 'htmlString', { fg = colors.base0A })
