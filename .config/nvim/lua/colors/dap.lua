9f46b02c43b6da6d81f0b72e057e6404a8c7b159
Vladislav Shavelskiy <v.shavelskiy@192.168.1.4>
2022-10-17 14:38:55 +0300

    update conifg

--- .config/nvim/lua/colors/dap.lua
+++ .config/nvim/lua/colors/dap.lua
@@ -1,10 +1,10 @@
-local hl = vim.highlight.create
+local hl = vim.api.nvim_set_hl
 local sg = vim.fn.sign_define
 local colors = require 'colors.colors'
 
-hl('DapBreakpoint', { ctermbg = 0, guifg = colors.base08, guibg = colors.base02 }, false)
-hl('DapLogPoint', { ctermbg = 0, guifg = colors.base0D, guibg = colors.base02 }, false)
-hl('DapStopped', { ctermbg = 0, guifg = colors.base0B, guibg = colors.base02 }, false)
+hl(0, 'DapBreakpoint', { fg = colors.base08, bg = colors.base02 })
+hl(0, 'DapLogPoint', { fg = colors.base0D, bg = colors.base02 })
+hl(0, 'DapStopped', { fg = colors.base0B, bg = colors.base02 })
 
 sg('DapBreakpoint', { text = '', texthl = 'DapBreakpoint', linehl = 'DapBreakpoint', numhl = 'DapBreakpoint' })
 sg(
